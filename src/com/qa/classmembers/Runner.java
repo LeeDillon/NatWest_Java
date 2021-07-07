d1770fab65d0ae36aa90737f3338426b4358c415
Lee-Dillon <lee.dillon1@outlook.com>
2021-07-07 12:36:10 +0100

    commiting calculator and results

--- src/com/qa/classmembers/Runner.java
+++ src/com/qa/classmembers/Runner.java
@@ -1,5 +1,25 @@
 package com.qa.classmembers;
 
 public class Runner {
-
+	static double Physics = 100;
+	static double Chemistry = 120;
+	static double Biology = 85;
+	static double Total  = Physics + Chemistry + Biology;
+	static double Percentage = (Total*100)/450;
+	
+	public static void main(String[] args) {
+		findTotal();
+		findPercentage();
+	}
+	
+	public static void findTotal() {
+		System.out.println("Your Total Score across all subjects is " +Total+" points");
+		
+		
+	}
+	public static void findPercentage() {
+		System.out.println("Your Total Score as a percentage is " + Percentage + "%");
+	
+	}
 }
+
---
+++ src/com/qa/classmembers/Runner2.java
@@ -0,0 +1,22 @@
+package com.qa.classmembers;
+
+public class Runner2 {public static void main(String[] args) {
+	
+	double Physics = 100;
+	double Chemistry = 120;
+	double Biology = 86;
+	double Total  = Physics + Chemistry + Biology;
+	double Percentage = Total / 450;
+
+	
+	System.out.println("Your Total Score across all subjects is " + Total + " Points");
+	
+	
+	System.out.println("Your Total Score as a percentage is " + Percentage + "%");
+	
+
+	
+	
+	}
+}
+
